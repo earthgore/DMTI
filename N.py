537dccf080eb2c590e08fa214f821da0cf088e4a
earthgore <102983873+earthgore@users.noreply.github.com>
2022-04-04 22:30:20 +0300

    Add files via upload

--- N.py
+++ N.py
@@ -2,4 +2,9 @@ class natural:
     def __init__(self, str):
         self.numbers = [int(i) for i in str]
         self.digit = len(str)
+    def __str__(self):
+        return ''.join([str(i) for i in self.numbers])
+
+
+
 
