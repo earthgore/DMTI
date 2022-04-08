af74f7cfa2166a261398c405a85f0dd6873b4cd4
slomfi <102984279+slomfi@users.noreply.github.com>
2022-04-08 16:22:51 +0300

    Add files via upload

--- Z.py
+++ Z.py
@@ -22,12 +22,12 @@ class integers:
 
 str0 = input("Введите первое число со знаком + или -:")
 z1 = integers(str0)
-str01 = input("Введите первое число со знаком + или -:")
+str01 = input("Введите второе число со знаком + или -:")
 z2 = integers(str01)
 
 # Абсолютная величина числа, результат - натуральное ABS_Z_N
 
-def ABS_Z_N1(z1):
+def ABS_Z_N(z1):
     i = 0
     k = 0
     x = 1
@@ -37,24 +37,11 @@ def ABS_Z_N1(z1):
         x = x * 10
     k = str(k)
     x = k[::-1]
-    print(x)
-    return x
-zx0 = ABS_Z_N1(z1)
-n1_1 = natural(zx0)
 
-def ABS_Z_N2(z2):
-    i = 0
-    k = 0
-    x = 1
-    while i < z2.digit:
-        k = k + z2.numbers[i] * x
-        i = i + 1
-        x = x * 10
-    k = str(k)
-    x = k[::-1]
-    print(x)
     return x
-zx = ABS_Z_N2(z2)
+zx0 = ABS_Z_N(z1)
+n1_1 = natural(zx0)
+zx = ABS_Z_N(z2)
 n1_2 = natural(zx)
 
 
@@ -94,25 +81,24 @@ def MUL_ZM_Z(z1):
     return k
 
 
-def TRANS_N_Z():
+def TRANS_N_Z(n):
     global z1_1
-    st1 = input("Введите первое число:")
-    n1 = natural(st1)
     i = 0
     k = 0
     x = 1
-    while i < n1.digit:
-        k = k + n1.numbers[i]*x
+    while i < n.digit:
+        k = k + n.numbers[i]*x
         i = i + 1
         x = x*10
     k = str(k)
     x = k[::-1]
     z1_1 = integers(x)
+    z1_1.sign = 0
     return x
 
 
 
-def TRANS_Z_N1(z1):
+def TRANS_Z_N(z1):
     if z1.sign == 1:
         return 505
     else:
@@ -125,81 +111,80 @@ def TRANS_Z_N1(z1):
         x = x * 10
     k = str(k)
     x = k[::-1]
+    x = natural(x)
     return x
 
-
-
-def TRANS_Z_N2(z2):
-    if z2.sign == 1:
-        return 505
-    else:
-     i = 0
-     k = 0
-     x = 1
-     while i < z2.digit:
-        k = k + z2.numbers[i] * x
-        i = i + 1
-        x = x * 10
-    k = str(k)
-    x = k[::-1]
-    return x
 
 
 def ADD_ZZ_Z(z1, z2):
-    global n1_1, n1_2
+    global n1_1, n1_2, z1_1
     x1 = POZ_Z_D(z1)
     x2 = POZ_Z_D(z2)
     if x1 == x2 and x1 == 2:
         x = ADD_NN_N(n1_1,n1_2)
-        x.sign = 0
+        TRANS_N_Z(x)
+        z1_1.sign = 0
     elif x1 == x2 and x1 == 0 or x2 == 0:
         if x1 == 0:
-            x = z2
+            z1_1 = z2
         else:
-            x = z1
+            z1_1 = z1
     elif x1 == x2 and x1 == 1:
         x = ADD_NN_N(n1_1,n1_2)
-        x.sign = 1
+        TRANS_N_Z(x)
+        z1_1.sign = 1
 
     else:
        k = COM_NN_D(n1_1,n1_2)
        if k == 2 and x1 == 1:
            x = SUB_NN_N(n1_1,n1_2)
-           x.sign = 1
+           TRANS_N_Z(x)
+           z1_1.sign = 1
+       elif k == 2 and x2 == 1:
+           x = SUB_NN_N(n1_1, n1_2)
+           TRANS_N_Z(x)
+           z1_1.sign = 0
        else:
-           x = SUB_NN_N(n1_1,n1_2)
-           x.sign = 0
-    return x
-
+           x = SUB_NN_N(n1_2,n1_1)
+           TRANS_N_Z(x)
+           z1_1.sign = 1
+    z1_1.showInfo()
+    return z1_1
 
 def SUB_ZZ_Z(z1, z2):
-    global n1_1, n1_2
+    global n1_1, n1_2, z1_1
     x1 = POZ_Z_D(z1)
     x2 = POZ_Z_D(z2)
-    k = COM_NN_D(n1, n2)
+    k = COM_NN_D(n1_1, n1_2)
     if x1 == x2 and x1 == 1:
         if k == 2:
             x = SUB_NN_N(n1_1, n1_2)
-            x.sign = 1
+            TRANS_N_Z(x)
+            z1_1.sign = 1
         else:
             x = SUB_NN_N(n1_2, n1_1)
-            x.sign = 0
+            TRANS_N_Z(x)
+            z1_1.sign = 0
     elif x1 == x2 and x1 == 2:
        if k == 2:
-        x = SUB_NN_N(n1_2, n1_1)
-        x.sign = 0
+        x = SUB_NN_N(n1_1, n1_2)
+        TRANS_N_Z(x)
+        z1_1.sign = 0
        else:
-        x = SUB_NN_N( n1_2, n1_1)
-        x.sign = 1
+        x = SUB_NN_N(n1_2, n1_1)
+        TRANS_N_Z(x)
+        z1_1.sign = 1
     elif x2 == 1 and x1 == 2:
         x = ADD_NN_N(n1_1,n1_2)
-        x.sign = 0
+        TRANS_N_Z(x)
+        z1_1.sign = 0
     else:
         x =  ADD_NN_N(n1_1,n1_2)
-        x.sign = 1
+        TRANS_N_Z(x)
+        z1_1.sign = 1
 
-    x.showInfo()
-    return x
+    z1_1.showInfo()
+    return z1_1
 
 
 def MUL_ZZ_Z(z1, z2):
@@ -208,15 +193,19 @@ def MUL_ZZ_Z(z1, z2):
     x2 = POZ_Z_D(z2)
     if x1 == x2 and x1 == 1:
         x = MUL_NN_N(n1_1,n1_2)
-        x.sign = 0
+        TRANS_N_Z(x)
+        z1_1.sign = 0
     elif x1 == x2 and x1 == 2:
         x = MUL_NN_N(n1_1,n1_2)
-        x.sign = 0
+        TRANS_N_Z(x)
+        z1_1.sign = 0
     else:
         x = MUL_NN_N(n1_1,n1_2)
-        x.sign = 1
-    x.showInfo()
-    return x
+        TRANS_N_Z(x)
+        z1_1.sign = 1
+    z1_1.showInfo()
+    return z1_1
+
 
 
 def DIV_ZZ_Z(z1, z2):
@@ -225,22 +214,25 @@ def DIV_ZZ_Z(z1, z2):
     x2 = POZ_Z_D(z2)
     if x1 == x2 and x1 == 1:
         x = DIV_NN_N(n1_1,n1_2)
-        x.sign = 0
+        TRANS_N_Z(x)
+        z1_1.sign = 0
     elif x1 == x2 and x1 == 2:
         x = DIV_NN_N(n1_1,n1_2)
-        x.sign = 0
+        TRANS_N_Z(x)
+        z1_1.sign = 0
     else:
         x = DIV_NN_N(n1_1,n1_2)
-        x.sign = 1
-    x.showInfo()
-    return x
+        TRANS_N_Z(x)
+        z1_1.sign = 1
+    z1_1.showInfo()
+    return z1_1
 
 
-
+"""
 def MOD_ZZ_Z(z1, z2):
     x = SUB_ZZ_Z(z1, z2 * DIV_ZZ_Z(z1, z2))
     x.sign = 0
     x.showInfo()
     return x
 
-zx = MOD_ZZ_Z(z1, z2)
+zx = MOD_ZZ_Z(z1, z2)"""
